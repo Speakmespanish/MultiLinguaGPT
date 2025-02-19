2e69c7a786e69504da156507530ea26e7b649bac
Adyson Vargas Medina <20220937@itla.edu.do>
2025-02-19 15:50:36 -0400

    Instalado paquete de AutoMapper en capa API

--- Backend/Backend-Core/UserService.Core/Interfaces/IUserRepository.cs
+++ Backend/Backend-Core/UserService.Core/Interfaces/IUserRepository.cs
@@ -3,5 +3,7 @@ public interface IUserRepository
     Task<User> GetUserByIdAsync(int id);
     Task<User> GetUserByEmailAsync(string email);
     Task<User> CreateUserAsync(User user);
+    Task<User> UpdateUserAsync(User user);
+    Task<bool> DeleteUserAsync(int id);
     
 }
