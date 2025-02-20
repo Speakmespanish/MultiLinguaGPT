f1763763bab914b94b327e21590ce8e3fc42901b
Adyson Vargas Medina <20220937@itla.edu.do>
2025-02-19 20:47:39 -0400

    Progreso en repositorio del UserService e implementacionn de encriptacion de Passwords en componente Auth capa Infraestructura

--- Backend/Backend-Core/UserService.Infrastructure/Repositories/UserRepository.cs
+++ Backend/Backend-Core/UserService.Infrastructure/Repositories/UserRepository.cs
@@ -14,12 +14,14 @@ public class UserRepository : IUserRepository
         return await _context.User.AnyAsync(u => u.Email == email);
     }
 
-    Task<User> GetUserByIdAsync(int id);
-    Task<User> GetUserByEmailAsync(string email);
+    //Task<User> GetUserByIdAsync(int id);
+    //Task<User> GetUserByEmailAsync(string email);
     //Task<User> CreateUserAsync(User user);
     Task<User> UpdateUserAsync(User user);
     Task<bool> DeleteUserAsync(int id);
 
+
+    //Hash Password Pending To Implement
     public async Task<User> CreateUserAsync (User user)
     {
         if (await ValidateUserExist(user.Email))
@@ -31,6 +33,29 @@ public class UserRepository : IUserRepository
         return user;
     }
 
+    public async Task<User> GetUserByIdAsync(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            throw new Exception("User not found");
+        }
+        return user;
+    }
+
+    public async Task<User> GetUserByEmailAsync(string email)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        if (user == null)
+        {
+            throw new Exception("User not found");
+        }
+        return user;
+    }
+    
     public async Task<User> UpdateUserAsync(User user)
+    {
+
+    }
 }
 
